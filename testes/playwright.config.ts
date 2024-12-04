797cb30e23e36e6ccf7d6031d5bab084494c74bb
kl3berjr <kleberjunior147@gmail.com>
2024-12-04 11:39:47 -0400

    up4

--- testes/playwright.config.ts
+++ testes/playwright.config.ts
@@ -40,11 +40,14 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
+    
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
@@ -55,17 +58,18 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-  
+
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
+    {
+       name: 'Microsoft Edge',
+       use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
--- testes/tests/example.spec.ts
+++ testes/tests/example.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('acessando o sid', async ({ page }) => {
-  await page.goto('https://sid.treinamento.ro.gov.br/');
-
+  await page.goto('https://sid.treinamento.ro.gov.br/'); //acesso ao sid
+  
+  
 });
+
+  
