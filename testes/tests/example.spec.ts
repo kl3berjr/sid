02144073a28acd590b71dadab39a8f06b0874c2b
kl3berjr <kleberjunior147@gmail.com>
2024-12-06 10:52:10 -0400

    up6

--- testes/tests/example.spec.ts
+++ testes/tests/example.spec.ts
@@ -19,11 +19,15 @@ test('acessando o sid', async ({ page }) => {
 
   await page.locator('xpath=/html/body/div/main/div[3]/div[2]/button').click(); //rh
 
-  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[3]/a').click();
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[3]/a').click(); // férias dps servidores
 
-  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[4]/a').click();
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[4]/a').click(); // portarias assinadas
 
-  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[6]/a').click();
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[6]/a').click(); //solicitações
 
-  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[7]/a').click();
-});
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[7]/a').click(); //mapa de férias
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[8]/a').click(); //relatório de férias
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[9]/a').click(); //assentamentos
+})
