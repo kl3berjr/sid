89f8aa8dfa17890de44801c915467b59a64ffa00
kl3berjr <kleberjunior147@gmail.com>
2024-12-06 10:41:18 -0400

    up5

--- testes/tests/example.spec.ts
+++ testes/tests/example.spec.ts
@@ -1,9 +1,29 @@
 import { test, expect } from '@playwright/test';
 
 test('acessando o sid', async ({ page }) => {
-  await page.goto('https://sid.treinamento.ro.gov.br/'); //acesso ao sid
+  await page.goto('https://sid.treinamento.ro.gov.br/');
   
-  
-});
+  await page.locator('xpath=/html/body/div/main/div[3]/a').click();
 
+  await page.locator('xpath=//*[@id="validation-login"]/div[1]/div').click();
+
+  await page.locator('xpath=/html/body/div[1]/div/div/div[3]/form/div[1]/div/input').fill("08636103665");
+
+  await page.locator('xpath=//*[@id="Password"]').click();
+
+  await page.locator('xpath=//*[@id="Password"]').fill("@setic2023");
+
+  await page.locator('xpath=//*[@id="validation-login"]/button').click();
   
+  await page.locator('xpath=/html/body/div/main/div[3]/div[1]/label/label').click();
+
+  await page.locator('xpath=/html/body/div/main/div[3]/div[2]/button').click(); //rh
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[3]/a').click();
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[4]/a').click();
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[6]/a').click();
+
+  await page.locator('xpath=/html/body/div[1]/div[3]/div[1]/div[4]/div[1]/ul/li[7]/a').click();
+});
